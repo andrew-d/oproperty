a425ec2d7dec95f4ddac33696317fe3a7fbe3547
Andrew Dunham <andrew@du.nham.ca>
2012-10-28 00:39:49 -0400

    Add implementation of oproperty

---
+++ oproperty.py
@@ -0,0 +1,156 @@
+
+class oproperty(object):
+    """
+    This class implements a property-like class that is designed to allow for
+    easy overriding of a base class's property.  This is especially useful for
+    things like mixins, where you don't necessarily know what the base class's
+    type is, and thus can't simply call BaseClass.prop.__set__.  And, since
+    super() doesn't proxy the __set__ function, it can be quite difficult to
+    override a base class's property while still conforming to DRY.
+
+    Usage:
+        class BaseClass(object):
+            @property
+            def prop(self):
+                return 1234
+
+        @property_overriding
+        class DerivedClass(object):
+            @oproperty
+            def prop(self, orig):
+                return orig() + 1
+
+
+    FAQ:
+        Q: Why is this necessary?
+        A: I like mixins, conceptually, but Python makes it a bit tricky to do
+           stuff like overriding a base class's property setter without
+           explicitly knowing what that class is.  I wrote this to simplify
+           things for me.
+
+        Q: How does it work?
+        A: In short, we pretend to be a property-like object, and instead of
+           raising an error if the get/set/delete method doesn't exist, we call
+           the next implementation found in a base class.  If the method *does*
+           exist, we call it, but also pass along a pointer to the original
+           method, so our overriding method can make use of the original
+           method, if it's necessary.
+
+        Q: Are there any caveats?
+        A: Maybe.  I haven't properly tested this with things like abstract
+           base classes (though I plan on doing so), and anything else that
+           might rely on something actually being a property object.  I'm
+           thinking of making this class derive from property, but I'm not sure
+           that's necessarily a great idea.  Testing will continue :-)
+
+        Q: What versions of Python does this work on?
+        A: This should work on Python 2.6+, including Python 3.
+    """
+
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None, name=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        self.__doc__ = doc
+
+        # If we're not explicitly given a name to override, we try and
+        # determine it by inspecting the names of any given functions.  If we
+        # can't do this, we raise an error, since we don't know what to
+        # override at all.
+        if name is None:
+            if fget is not None:
+                name = fget.__name__
+            elif fset is not None:
+                name = fset.__name__
+            elif fdel is not None:
+                name = fdel.__name__
+            else:
+                raise RuntimeError("Can't create a property with no functions!")
+
+        self._prop_name = name
+        self.__class_type = None
+
+    def __get__(self, obj, type=None):
+        # If we have no object, return ourself.
+        if obj is None:
+            return self
+
+        # Get the superclass's attribute.
+        super_attr = self._get_super_attribute(obj, self._prop_name)
+
+        # If we have an attribute, call and return it.  Otherwise, we simply
+        # call the base property's __get__ function.
+        if self.fget is not None:
+            return self.fget(obj, lambda: super_attr.__get__(obj))
+        else:
+            return super_attr.__get__(obj)
+
+    def __set__(self, obj, value):
+        # Get the superclass's attribute.
+        super_attr = self._get_super_attribute(obj, self._prop_name)
+
+        # If we have an attribute, call and return it.  Otherwise, we simply
+        # call the base property's __set__ function.
+        if self.fset is not None:
+            return self.fset(obj, value, lambda val: super_attr.__set__(obj, val))
+        else:
+            return super_attr.__set__(obj, value)
+
+    def __delete__(self, obj):
+        # Get the superclass's attribute.
+        super_attr = self._get_super_attribute(obj, self._prop_name)
+
+        # If we have an attribute, call and return it.  Otherwise, we simply
+        # call the base property's __delete__ function.
+        if self.fdel is not None:
+            return self.fdel(obj, lambda val: super_attr.__delete__(obj))
+        else:
+            return super_attr.__delete__(obj)
+
+    def set_class_type(self, klass):
+        """
+        This function is called by our decorator, below, to tell this class
+        where to start searching in the __mro__ list.
+        """
+        self.__class_type = klass
+
+    def _get_super_attribute(self, obj, name):
+        if self.__class_type is None:
+            raise RuntimeError("You must decorate class!")
+
+        # Get the MRO for the object
+        if isinstance(obj, type):
+            mro = obj.__mro__
+        else:
+            mro = obj.__class__.__mro__
+
+        # Find this class in the MRO.
+        for pos in xrange(len(mro)):
+                if mro[pos] == self.__class_type:
+                        break
+
+        # Look through classes higher in the MRO for this attribute.
+        for pos in xrange(pos + 1, len(mro)):
+                tmp = mro[pos]
+
+                if isinstance(tmp, type) and name in tmp.__dict__:
+                    return tmp.__dict__[name]
+
+        return None
+
+    def setter(self, fset):
+        self.fset = fset
+        return self
+
+    def deleter(self, fdel):
+        self.fdel = fdel
+        return self
+
+
+def property_overriding(klass):
+    for name, val in klass.__dict__.items():
+        if isinstance(val, oproperty):
+            val.set_class_type(klass)
+
+    return klass
+
---
+++ tests.py
@@ -0,0 +1,199 @@
+import unittest
+from testing_helpers import BaseTestCase
+from oproperty import *
+
+# Generic base class
+class BaseClass(object):
+    def __init__(self):
+        self.val = 123
+
+    @property
+    def prop(self):
+        return self.val
+
+    @prop.setter
+    def prop(self, val):
+        self.val = val
+
+    @prop.deleter
+    def prop(self):
+        self.val = 0
+
+
+class TestBasicImplementation(BaseTestCase):
+    def setup(self):
+        @property_overriding
+        class DerivedClass(BaseClass):
+            @oproperty
+            def prop(self, orig):
+                return orig() + 1
+
+            @prop.setter
+            def prop(self, val, orig):
+                orig(val - 10)
+
+        self.BaseClass = BaseClass
+        self.DerivedClass = DerivedClass
+        self.b = BaseClass()
+        self.d = DerivedClass()
+
+    def test_simple_get(self):
+        self.assert_equal(self.d.prop, self.d.val + 1)
+
+    def test_simple_set(self):
+        self.d.prop = 555
+        self.assert_equal(self.d.val, 555 - 10)
+
+    def test_base_can_be_called(self):
+        self.assert_equal(self.BaseClass.prop.__get__(self.d), self.d.val)
+        self.BaseClass.prop.__set__(self.d, 444)
+        self.assert_equal(self.d.val, 444)
+
+
+class TestNamings(BaseTestCase):
+    def build_class(self, **kwargs):
+        class BaseClass(object):
+            @property
+            def prop1(self):
+                return 123
+
+            @property
+            def prop2(self):
+                return 456
+
+        @property_overriding
+        class DerivedClass(BaseClass):
+            prop = oproperty(**kwargs)
+
+        return DerivedClass()
+
+    def test_fget_name(self):
+        def prop1(self, orig):
+            return orig()
+
+        o = self.build_class(fget=prop1)
+        self.assert_equal(o.prop, 123)
+
+    def test_fset_name(self):
+        def prop1(): pass
+        o = self.build_class(fset=prop1)
+        self.assert_equal(o.prop, 123)
+
+    def test_fdel_name(self):
+        def prop1(): pass
+        o = self.build_class(fdel=prop1)
+        self.assert_equal(o.prop, 123)
+
+    def test_name_override(self):
+        def not_a_prop(self, orig):
+            return orig()
+
+        o = self.build_class(name='prop2')
+        self.assert_equal(o.prop, 456)
+
+    def test_fails_if_no_name(self):
+        with self.assert_raises(RuntimeError):
+            o = self.build_class()
+
+
+class TestMiscellaneous(BaseTestCase):
+    def test_with_mixin(self):
+        class BaseClass1(object):
+            @property
+            def prop(self):
+                return 123
+
+        class BaseClass2(object):
+            @property
+            def prop(self):
+                return 456
+
+        @property_overriding
+        class MyMixin(object):
+            @oproperty
+            def prop(self, orig):
+                return orig() + 5
+
+        class MixedIn1(MyMixin, BaseClass1):
+            pass
+
+        class MixedIn2(MyMixin, BaseClass2):
+            pass
+
+        m1 = MixedIn1()
+        m2 = MixedIn2()
+
+        self.assert_equal(m1.prop, 123 + 5)
+        self.assert_equal(m2.prop, 456 + 5)
+
+    def test_multiple_overriding(self):
+        @property_overriding
+        class Derived1(BaseClass):
+            @oproperty
+            def prop(self, orig):
+                return orig() + 1
+
+        @property_overriding
+        class Derived2(Derived1):
+            @oproperty
+            def prop(self, orig):
+                return orig() + 5
+
+        d1 = Derived1()
+        d2 = Derived2()
+
+        self.assert_equal(d1.prop, 123 + 1)
+        self.assert_equal(d2.prop, 123 + 1 + 5)
+
+    def test_with_other_overriding(self):
+        @property_overriding
+        class Derived1(BaseClass):
+            @oproperty
+            def prop(self, orig):
+                return orig() + 1
+
+        class Derived2(Derived1):
+            pass
+
+        d2 = Derived2()
+        self.assert_equal(d2.prop, 123 + 1)
+
+    def test_readonly(self):
+        @property_overriding
+        class Derived(BaseClass):
+            @oproperty
+            def readonly(self, orig):
+                return orig() + 1
+
+        d = Derived()
+        with self.assert_raises(AttributeError):
+            d.readonly = 'moo'
+
+    def test_without_decorator(self):
+        # @property_overriding      # NOTE: left out deliberately
+        class Derived(BaseClass):
+            @oproperty
+            def prop(self, orig):
+                return orig() + 1
+
+        d = Derived()
+        with self.assert_raises(RuntimeError):
+            v = d.prop
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestBasicImplementation))
+    suite.addTest(unittest.makeSuite(TestNamings))
+    suite.addTest(unittest.makeSuite(TestMiscellaneous))
+
+    return suite
+
+def main():
+    unittest.main(defaultTest='suite', exit=False)
+    print("Number of assertions: {0}".format(BaseTestCase.number_of_assertions))
+    print("")
+
+if __name__ == '__main__':
+    main()
+
